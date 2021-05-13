b13dae5e17427228677ec3dd99fe5de8988fbefa
javmarina <javmarina@uma.es>
2021-05-13 19:42:31 +0200

    Minor refactor of signaling logic - Moved ClientSocket class into SignalingPeer.java - Extracted constants for better maintainability

--- gui/WebRTC/src/main/java/com/javmarina/webrtc/signaling/SignalingPeer.java
+++ gui/WebRTC/src/main/java/com/javmarina/webrtc/signaling/SignalingPeer.java
@@ -5,6 +5,10 @@ import dev.onvoid.webrtc.RTCIceCandidate;
 import dev.onvoid.webrtc.RTCSessionDescription;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.json.JSONObject;
@@ -20,9 +24,15 @@ public class SignalingPeer {
     // TODO: use wss://
     private static final String WEBSOCKET_ADDRESS = "ws://nintendo-switch-remote-control.appspot.com/signaling";
 
-    public static final String OFFER_COMMAND = "offer";
-    public static final String ANSWER_COMMAND = "answer";
-    public static final String NEW_ICE_CANDIDATE_COMMAND = "candidate";
+    private static final String COMMAND_REGISTER_OK = "register-ok";
+    private static final String COMMAND_REGISTER_INVALID = "register-invalid";
+    private static final String COMMAND_OFFER = "offer";
+    private static final String COMMAND_ANSWER = "answer";
+    private static final String COMMAND_NEW_ICE_CANDIDATE = "candidate";
+
+    private static final String JSON_COMMAND = "command";
+    private static final String JSON_SESSION_ID = "session-id";
+    private static final String JSON_PAYLOAD = "payload";
 
     private final SessionId sessionId;
     private final HttpClient httpClient;
@@ -62,7 +72,7 @@ public class SignalingPeer {
         }
     }
 
-    public void connect(final SignalingPeer.Callback callback) throws Exception {
+    public void connect(final Callback callback) throws Exception {
         final ClientUpgradeRequest request = new ClientUpgradeRequest();
         // Attempt connection
         final Future<Session> future = webSocketClient.connect(
@@ -89,31 +99,82 @@ public class SignalingPeer {
 
     public void sendOffer(final RTCSessionDescription description) throws IOException {
         sendCommand(
-                OFFER_COMMAND,
+                COMMAND_OFFER,
                 JsonCodec.encode(description)
         );
     }
 
     public void sendAnswer(final RTCSessionDescription description) throws IOException {
         sendCommand(
-                ANSWER_COMMAND,
+                COMMAND_ANSWER,
                 JsonCodec.encode(description)
         );
     }
 
     public void sendIceCandidate(final RTCIceCandidate candidate) throws IOException {
         sendCommand(
-                NEW_ICE_CANDIDATE_COMMAND,
+                COMMAND_NEW_ICE_CANDIDATE,
                 JsonCodec.encode(candidate)
         );
     }
 
     public void sendCommand(final String commandId, final JSONObject payload) throws IOException {
         final JSONObject jo = new JSONObject();
-        jo.put("command", commandId);
-        jo.put("session-id", sessionId.id);
-        jo.put("payload", payload);
+        jo.put(JSON_COMMAND, commandId);
+        jo.put(JSON_SESSION_ID, sessionId.id);
+        jo.put(JSON_PAYLOAD, payload);
         sendMessage(jo.toString());
+    }
+
+    @SuppressWarnings("unused")
+    @WebSocket(maxTextMessageSize = 64 * 1024)
+    public static final class ClientSocket {
+
+        private final Callback callback;
+        private Session session;
+
+        ClientSocket(final Callback callback) {
+            this.callback = callback;
+        }
+
+        @OnWebSocketClose
+        public void onClose(final int statusCode, final String reason) {
+            this.session = null;
+        }
+
+        @OnWebSocketConnect
+        public void onConnect(final Session session) {
+            this.session = session;
+        }
+
+        @OnWebSocketMessage
+        public void onMessage(final String msg) {
+            final JSONObject jo = new JSONObject(msg);
+            final String command = jo.getString(JSON_COMMAND);
+            switch (command) {
+                case COMMAND_REGISTER_INVALID:
+                    session.close();
+                    callback.onInvalidRegister();
+                    break;
+                case COMMAND_REGISTER_OK:
+                    break;
+                case COMMAND_OFFER:
+                case COMMAND_ANSWER:
+                    final RTCSessionDescription description =
+                            JsonCodec.decodeSessionDescription(jo.getJSONObject(JSON_PAYLOAD));
+                    if (COMMAND_OFFER.equals(command)) {
+                        callback.onOfferReceived(description);
+                    } else {
+                        callback.onAnswerReceived(description);
+                    }
+                    break;
+                case COMMAND_NEW_ICE_CANDIDATE:
+                    final RTCIceCandidate candidate =
+                            JsonCodec.decodeCandidate(jo.getJSONObject(JSON_PAYLOAD));
+                    callback.onCandidateReceived(candidate);
+                    break;
+            }
+        }
     }
 
     public interface Callback {
