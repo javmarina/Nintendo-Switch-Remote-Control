6a1c5321b08a9ee20a415a288c0b8a1e843c450e
javmarina <javmarina@uma.es>
2021-05-21 19:42:42 +0200

    Fixed visual artifacts in PanelController.java

--- gui/server/src/main/java/com/javmarina/server/PanelController.java
+++ gui/server/src/main/java/com/javmarina/server/PanelController.java
@@ -57,28 +57,30 @@ public class PanelController {
     private Button lostConnection;
 
     public void updateUi(final Packet packet) {
-        a.setFill(packet.getButtons().getA() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        b.setFill(packet.getButtons().getB() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        x.setFill(packet.getButtons().getX() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        y.setFill(packet.getButtons().getY() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        l.setFill(packet.getButtons().getL() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        zl.setFill(packet.getButtons().getZl() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        r.setFill(packet.getButtons().getR() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        zr.setFill(packet.getButtons().getZr() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        plus.setFill(packet.getButtons().getPlus() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        minus.setFill(packet.getButtons().getMinus() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        home.setFill(packet.getButtons().getHome() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        capture.setFill(packet.getButtons().getCapture() ? Color.DARKGRAY : Color.LIGHTGRAY);
+        Platform.runLater(() -> {
+            a.setFill(packet.getButtons().getA() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            b.setFill(packet.getButtons().getB() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            x.setFill(packet.getButtons().getX() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            y.setFill(packet.getButtons().getY() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            l.setFill(packet.getButtons().getL() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            zl.setFill(packet.getButtons().getZl() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            r.setFill(packet.getButtons().getR() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            zr.setFill(packet.getButtons().getZr() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            plus.setFill(packet.getButtons().getPlus() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            minus.setFill(packet.getButtons().getMinus() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            home.setFill(packet.getButtons().getHome() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            capture.setFill(packet.getButtons().getCapture() ? Color.DARKGRAY : Color.LIGHTGRAY);
 
-        dpadUp.setFill(packet.getDpad().getUp() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        dpadRight.setFill(packet.getDpad().getRight() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        dpadDown.setFill(packet.getDpad().getDown() ? Color.DARKGRAY : Color.LIGHTGRAY);
-        dpadLeft.setFill(packet.getDpad().getLeft() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            dpadUp.setFill(packet.getDpad().getUp() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            dpadRight.setFill(packet.getDpad().getRight() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            dpadDown.setFill(packet.getDpad().getDown() ? Color.DARKGRAY : Color.LIGHTGRAY);
+            dpadLeft.setFill(packet.getDpad().getLeft() ? Color.DARKGRAY : Color.LIGHTGRAY);
 
-        leftJoystick.setFill(packet.getButtons().getLclick() ? Color.GREEN : Color.DARKGRAY);
-        rightJoystick.setFill(packet.getButtons().getRclick() ? Color.GREEN : Color.DARKGRAY);
-        setJoystickPosition(leftJoystick, packet.getLeftJoystick());
-        setJoystickPosition(rightJoystick, packet.getRightJoystick());
+            leftJoystick.setFill(packet.getButtons().getLclick() ? Color.GREEN : Color.DARKGRAY);
+            rightJoystick.setFill(packet.getButtons().getRclick() ? Color.GREEN : Color.DARKGRAY);
+            setJoystickPosition(leftJoystick, packet.getLeftJoystick());
+            setJoystickPosition(rightJoystick, packet.getRightJoystick());
+        });
     }
 
     private static void setJoystickPosition(final Circle circle, final Packet.Joystick joystick) {
